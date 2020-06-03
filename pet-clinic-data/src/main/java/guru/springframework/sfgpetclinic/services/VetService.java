16a87be1eff23571ad93823547a2e748678d92fc
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-03 14:56:34 -0300

    Criada interface CrudService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,12 +2,4 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long> { }
